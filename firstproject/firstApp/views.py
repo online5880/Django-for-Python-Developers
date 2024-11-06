dac811b48eef0810f5c70db2db2bb1629a3b0be0
online5880 <gjtjqkr1@naver.com>
2024-11-07 01:10:35 +0900

    django 기본
    - 프로젝트 생성
    - 앱 생성

---
+++ firstproject/firstApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+# Create your views here.
+def display(request):
+    return HttpResponse("<h1>My First Django App!!</h1>")
+
+def displayDateTime(request):
+    dt=datetime.datetime.now()
+    s="<b>Current Date And Time:</b>"+str(dt)
+    return HttpResponse(s)
