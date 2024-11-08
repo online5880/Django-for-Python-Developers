4438adbae6144bc8c270befdf945b5595f1fbb69
online5880 <gjtjqkr1@naver.com>
2024-11-09 01:39:29 +0900

    Django ORM

--- modelDemo/modelDemo/settings.py
+++ modelDemo/modelDemo/settings.py
@@ -83,6 +83,13 @@ DATABASES = {
         'PASSWORD':'1234',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 
 # Password validation
